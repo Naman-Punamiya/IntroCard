302532593a38995f68206383587964daf6402daa
Naman Punamiya <namanpunamiya6@gmail.com>
2025-06-14 23:13:14 +0530

    Add the App First Time

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,6 @@ class CardConnect extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: HomeScreen()
-  //       Scaffold(
-  //     appBar: AppBar(
-  //       title: Text(
-  //         'First App',
-  //       ),
-  //       backgroundColor: Colors.cyan[800],
-  //       centerTitle: true,
-  //       foregroundColor: Colors.white,
-  //     ),
-  //     backgroundColor: Colors.cyan[200],
-  //     body: Padding(
-  //       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-  //       child: ListView(
-  //         scrollDirection: Axis.horizontal,
-  //         children: [
-  //           Image(image: AssetImage('images/GTR_Front_Angle.png'),width: 400,),
-  //           Image(image: AssetImage('images/GTR_Front.png'),width: 400,),
-  //           Image(image: AssetImage('images/GTR_Left.webp'),width: 400,),
-  //           Image(image: AssetImage('images/GTR_Right.webp'),width: 400,),
-  //           Image(image: AssetImage('images/GTR_Back.png'),width: 400,),
-  //         ],
-  //       )
-  //     ),
-    // )
     );
   }
 }
